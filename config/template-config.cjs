2e434ed8d7d23facd0c56455c895a202a8f036e4
nbspnumb <48712508+nbspnumb@users.noreply.github.com>
2023-07-05 15:35:54 +0800

    Update template-config.cjs
    0003消息模板更新

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -58,14 +58,18 @@ const TEMPLATE_CONFIG = [
     desc: `
        🗓️{{date.DATA}}
 
-       今天是我们在一起的第{{love_day.DATA}}天，爱你❤️
+       ❤️今天是我们恋爱的第{{love_day.DATA}}天，爱你❤️
 
-       今日{{city.DATA}}天气☁️：{{tian_api_weather_weather_0.DATA}}
-       温度🌡️: {{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
-       紫外线强度指数☀️: {{tian_api_weather_uv_index_0.DATA}}
-       穿衣指数👚: {{tian_api_weather_tips_0.DATA}}
-
+       今日{{city.DATA}}天气☁️：{{weather.DATA}}
+       温度🌡️: {{min_temperature.DATA}}-{{max_temperature.DATA}}
+       天气温馨语💝:{{notice.DATA}}
+       
+       🎂{{birthday_message.DATA}}
        💌{{earthy_love_words.DATA}}
+       
+       {{note_en.DATA}}  
+       {{note_ch.DATA}}
+
     `
   },
 ]
