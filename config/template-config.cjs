a9e240ff931ad888c2a1f15399c2e988d6bd9b02
nbspnumb <48712508+nbspnumb@users.noreply.github.com>
2023-07-05 10:31:13 +0800

    Update template-config.cjs
    新增0003消息模板，适用于PushDeer推送服务

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -52,6 +52,22 @@ const TEMPLATE_CONFIG = [
       失败用户: {{fail_post_ids.DATA}}
     `
   },
+    {
+    id: '0003',
+    title: '乔宝贝，早上好！',
+    desc: `
+       🗓️{{date.DATA}}
+
+       今天是我们在一起的第{{love_day.DATA}}天，爱你❤️
+
+       今日{{city.DATA}}天气☁️：{{tian_api_weather_weather_0.DATA}}
+       温度🌡️: {{tian_api_weather_lowest_0.DATA}}-{{tian_api_weather_highest_0.DATA}}
+       紫外线强度指数☀️: {{tian_api_weather_uv_index_0.DATA}}
+       穿衣指数👚: {{tian_api_weather_tips_0.DATA}}
+
+       💌{{earthy_love_words.DATA}}
+    `
+  },
 ]
 
 module.exports = TEMPLATE_CONFIG
