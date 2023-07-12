7a56d414be3a9400d7d32ebc6d4c88fb201bb0f9
nbspnumb <48712508+nbspnumb@users.noreply.github.com>
2023-07-12 11:31:31 +0800

    Update index.cjs
    bug-fix

--- config/index.cjs
+++ config/index.cjs
@@ -25,7 +25,7 @@ const USER_CONFIG = {
       name: '乔宝贝',
 
       // 使用微信测试号推送消息，参数配置
-      id: {'oUOIJ6vJrI6ukspNM084ZQ5H-_QM','oUOIJ6vJrI6ukspNM084ZQ5H-_QM'},
+      id: 'oUOIJ6vJrI6ukspNM084ZQ5H-_QM',
       useTemplateId: 'mF5nscQZ7fASzxydG2omCZkT8Z5EPPookc9AE6ku-DI',
       
       // 使用push-deer推送消息，参数配置
@@ -55,6 +55,43 @@ const USER_CONFIG = {
         { keyword: 'marry_day', date: '2023-06-28' },
       ],
     },
+
+    {
+      // 想要发送的人的名字
+      name: '乔宝贝',
+
+      // 使用微信测试号推送消息，参数配置
+      id: 'oUOIJ6vJrI6ukspNM084ZQ5H-_QM',
+      useTemplateId: 'mF5nscQZ7fASzxydG2omCZkT8Z5EPPookc9AE6ku-DI',
+      
+      // 使用push-deer推送消息，参数配置
+      // id: 'PDU23816Tgku1Bzl2gmebxtOftUIGL5Kr9RI0HV6y',
+      // useTemplateId: '0003',
+      
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-05',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '宝贝', year: '1998', date: '01-09',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '宝贝', year: '1998', date: '02-05',
+        },
+        {
+          type: '节日', name: '相识纪念日', year: '2023', date: '06-04',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2023-06-24' },
+        // 结婚纪念日
+        { keyword: 'marry_day', date: '2023-06-28' },
+      ],
+    },
+    
   ],
 
 
