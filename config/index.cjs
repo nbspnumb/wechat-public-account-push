86616f3b80dbe24ca2caad85caabf368a20aeac8
nbspnumb <48712508+nbspnumb@users.noreply.github.com>
2023-07-24 14:58:39 +0800

    Update index.cjs
    仅推送消息至郑猛男

--- config/index.cjs
+++ config/index.cjs
@@ -25,37 +25,37 @@ const USER_CONFIG = {
   CITY: '阜阳',
 
   USERS: [
-    {
-      // 想要发送的人的名字
-      name: '乔宝贝',
+    // {
+    //   // 想要发送的人的名字
+    //   name: '乔宝贝',
 
-      // 使用push-deer推送消息，参数配置
-      id: 'PDU24085TUANRUdf9KZuqn4n4mzpQ4MLojyRGrX7q',
-      useTemplateId: '0003',
+    //   // 使用push-deer推送消息，参数配置
+    //   id: 'PDU24085TUANRUdf9KZuqn4n4mzpQ4MLojyRGrX7q',
+    //   useTemplateId: '0003',
       
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '02-05',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1998', date: '01-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '宝贝', year: '1998', date: '02-05',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2023', date: '06-04',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2023-06-24' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2023-06-28' },
-      ],
-    },
+    //   // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+    //   horoscopeDate: '02-05',
+    //   festivals: [
+    //     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+    //     {
+    //       type: '*生日', name: '宝贝', year: '1998', date: '01-09',
+    //     },
+    //     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+    //     {
+    //       type: '生日', name: '宝贝', year: '1998', date: '02-05',
+    //     },
+    //     {
+    //       type: '节日', name: '相识纪念日', year: '2023', date: '06-04',
+    //     },
+    //   ],
+    //   // 我们在一起已经有xxxx天了的配置
+    //   customizedDateList: [
+    //     // 在一起的日子
+    //     { keyword: 'love_day', date: '2023-06-24' },
+    //     // 结婚纪念日
+    //     { keyword: 'marry_day', date: '2023-06-28' },
+    //   ],
+    // },
 
 
     {
